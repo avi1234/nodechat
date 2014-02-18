b53064e04978ceb9452fa99fa3d8d0ac4e3123bc
Avi <avikadar@Avis-MacBook-Pro.local>
2014-02-19 00:00:37 +0200

    bla

--- server.js
+++ server.js
@@ -1,5 +1,57 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var mime = require('mime');
-var cache = {};
+var http             = require('http');
+var fs               = require('fs');
+var path             = require('path');
+var mime             = require('mime');
+var cache            = {};
+
+function send404(res){
+	res.writeHead(404, {'Content-Type': 'text/plain'});
+	res.write('Error 404: resource not found.');
+	res.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+	response.writeHead(200,{"content-type": mime.lookup(path.basename(filePath))});
+	response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+	if (cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
+	} else {
+		fs.exists(absPath, function(exists) {
+			if (exists) {
+				fs.readFile(absPath, function(err, data) {
+					if (err) {
+						send404(response);
+					} else {
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			} else {
+				send404(response);
+			}
+		}); 
+	}
+}
+
+var server = http.createServer(function(req,res){
+	var filePath = false;
+	if(req.url == '/') {
+		filePath = 'public/index.html';
+	} else {
+		filePath = 'public' + req.url;
+	}
+	
+	var absPath = './' + filePath;
+	serveStatic(res,cache,absPath);
+}).listen(3000);
+
+console.log("server running");
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+
+
+	
